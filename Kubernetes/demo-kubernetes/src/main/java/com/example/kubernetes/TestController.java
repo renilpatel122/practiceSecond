dac4903e6d80c1bd1fa8bde95269e3bcf9178c36
renilpatel122 <renil.itidol@outlook.com>
2025-08-21 13:50:55 +0530

    Add Kubernetes project

---
+++ Kubernetes/demo-kubernetes/src/main/java/com/example/kubernetes/TestController.java
@@ -0,0 +1,12 @@
+package com.example.kubernetes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api")
+    public String hello() {
+        return "Hello From Kubernetes";
+    }
+}
